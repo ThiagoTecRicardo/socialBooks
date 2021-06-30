d8b2b5332500954e9cedc0bb1037a5a900d8249a
Thiago Ricardo <thiago.tec.ricardou@gmail.com>
2021-06-30 20:53:50 -0300

    First commit

---
+++ src/main/java/br/com/livros/socialbooks/domain/Livro.java
@@ -0,0 +1,109 @@
+package br.com.livros.socialbooks.domain;
+
+import java.util.Date;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+public class Livro {
+
+	@JsonInclude(Include.NON_NULL)
+	private Long Id;
+	
+	@JsonInclude(Include.NON_NULL)
+	private String nome;
+
+	@JsonInclude(Include.NON_NULL)
+	private String autor;
+
+	@JsonInclude(Include.NON_NULL)
+	private Date publicacao;
+
+	@JsonInclude(Include.NON_NULL)
+	private String resumo;
+
+	@JsonInclude(Include.NON_NULL)
+	private List<Comentario> comentarios;
+
+	public Livro() {
+	}
+
+	public Livro(String nome) {
+		this.nome = nome;
+	}
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public Date getPublicacao() {
+		return publicacao;
+	}
+
+	public void setPublicacao(Date publicacao) {
+		this.publicacao = publicacao;
+	}
+
+	public String getResumo() {
+		return resumo;
+	}
+
+	public void setResumo(String resumo) {
+		this.resumo = resumo;
+	}
+
+	public List<Comentario> getComentarios() {
+		return comentarios;
+	}
+
+	public void setComentarios(List<Comentario> comentarios) {
+		this.comentarios = comentarios;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((Id == null) ? 0 : Id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Livro other = (Livro) obj;
+		if (Id == null) {
+			if (other.Id != null)
+				return false;
+		} else if (!Id.equals(other.Id))
+			return false;
+		return true;
+	}
+
+}

5d214ca1a6e32051e3586d00898eb15a439f3e66
Thiago Ricardo <thiago.tec.ricardou@gmail.com>
2021-07-01 12:51:26 -0300

    Tratamento correto das respostas HTTP 404 e 201

--- src/main/java/br/com/livros/socialbooks/resources/LivrosResources.java
+++ src/main/java/br/com/livros/socialbooks/resources/LivrosResources.java
@@ -1,7 +1,9 @@
 package br.com.livros.socialbooks.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 import br.com.livros.socialbooks.domain.Livro;
 import br.com.livros.socialbooks.repository.LivrosRepository;
 
@@ -27,16 +31,23 @@ public class LivrosResources {
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	public void save(@RequestBody Livro livro) {
-
-		livrosRepository.save(livro);
-
+	public ResponseEntity<Void> salvar(@RequestBody Livro livro) {
+		
+		livro = livrosRepository.save(livro);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("{/id}").buildAndExpand(livro.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
 	}
 	
 	@RequestMapping(value = "/{id}" , method = RequestMethod.GET)
 	public ResponseEntity<Livro> buscar(@PathVariable Long id) {
 		
 		Optional<Livro> livro = livrosRepository.findById(id);
+		
+		if(livro.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		}
 		
 		return ResponseEntity.ok(livro.get());
 	}
@@ -49,7 +60,7 @@ public class LivrosResources {
 	}
 	
 	@RequestMapping(value = "/{id}" , method = RequestMethod.PUT)
-	public void deletar(@RequestBody Livro livro, @PathVariable Long id ) {
+	public void atualizar(@RequestBody Livro livro, @PathVariable Long id ) {
 		
 		livro.setId(id);
 		

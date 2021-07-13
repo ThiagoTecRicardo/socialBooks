d3658b84596ef2cbc9db1b02f7429d2912388fbe
Thiago Ricardo <thiago.tec.ricardou@gmail.com>
2021-07-13 13:14:27 -0300

    configurando cors

--- src/main/java/br/com/livros/socialbooks/resources/LivrosResources.java
+++ src/main/java/br/com/livros/socialbooks/resources/LivrosResources.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.CacheControl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,6 +34,7 @@ public class LivrosResources {
 	@Autowired
 	private LivrosRepository livrosRepository;
 
+	@CrossOrigin
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<Livro>> listar() {
 		
